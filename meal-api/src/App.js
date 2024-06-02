ae9d2cdc751fa4688ee2e73cd6424decac74e353
prashant-sagar-shakya <prashantshakya43602003@gmail.com>
2024-06-02 16:19:32 +0530

    Third Project Meal App

--- meal-api/src/App.js
+++ meal-api/src/App.js
@@ -1,7 +1,7 @@
-import "./App.css";
+import Main from "./Main";
 
 function App() {
-  return <div>Hello World!</div>;
+  return <Main />;
 }
 
 export default App;
---
+++ meal-api/src/Main.js
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "./style.css";
+function Main() {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood")
+      .then((res) => {
+        console.log(res.data);
+        setItems(res.data.meals);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  const itemsList = items.map(({ strMeal, strMealThumb, idMeal }) => {
+    return (
+      <section className="card">
+        <img src={strMealThumb} alt={strMeal} />
+        <section className="content">
+          <p>{strMeal}</p>
+          <p>
+            <div>{idMeal}</div>
+          </p>
+        </section>
+      </section>
+    );
+  });
+  return <div className="items-container">{itemsList}</div>;
+}
+
+export default Main;
